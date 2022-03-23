7df4136bcdf6cd9a014ef4f710f500d637014a89
“Michael-kestler” <michael.s.kestler@gmail.com>
2022-03-23 10:25:25 -0700

    added exercise 6

--- code.js
+++ code.js
@@ -3,14 +3,16 @@ const helloWorld = function(){
     return "Hello, World!";
 }
 function sayHello(input){
-    if (input === "Alex"){
-        return "Hello, Alex!";
-    } else if (input === "Pat"){
-        return "Hello, Pat!";
-    }
-    else {
-        return "Hello, Jane!";
-    }
+    return "Hello,  " + input + "!";
+
+    // if (input === "Alex"){
+    //     return "Hello, Alex!";
+    // } else if (input === "Pat"){
+    //     return "Hello, Pat!";
+    // }
+    // else {
+    //     return "Hello, Jane!";
+    // }
 
 }
 
